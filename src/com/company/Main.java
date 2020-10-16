a8c4499b48e55361b5fdd0f32d5c6bce5ee811c1
alisa-one <72848258+alisa-one@users.noreply.github.com>
2020-10-17 02:14:18 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//
+
+         final int NUM=100;
+        String word="м" +"  уровень над обьектом";
+        String levelOfSea=NUM+word;
+        System.out.println(levelOfSea+""+""+NUM+""+""+word);
+        if(NUM<0){System.out.println("Вы сохранили отрицательное число");
+        }
+else if(NUM>0){System.out.println("ВЫ сохранили положительное число");
+}else  {System.out.println("Вы сохранили нуль");}
+  ;}  ;
+
+}
+
+
+
+
+
+
